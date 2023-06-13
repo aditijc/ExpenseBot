d9bf3a638ec4d673c44b8dbcbcd94e029fdc1ccb
Aditi Chandrashekar <aditijc@gmail.com>
2023-06-12 17:27:56 -0700

    Incorporate Budget Tracking

--- bot.py
+++ bot.py
@@ -11,17 +11,37 @@ intents.presences = True
 intents.message_content = True
 
 CATEGORIES = ["Groceries", "Clothes", "Hygiene", "Food", "Events"]
+# TODO: Change categories to read from the column names
 
 bot = commands.Bot(command_prefix='--', intents=intents)
 
 @bot.command()
 async def info(ctx):
-    await ctx.send(f"Existing Categories are: {CATEGORIES}\n\nCommands include:\n--info\n--spend <date> <category> <amount> <description>\n--expenses\n--expense_log\n--reset")
+    await ctx.send(f"Existing Categories are: {CATEGORIES}\n\n\
+                   Commands include:\
+                   \n--info\
+                   \n--spend <date> <category> <amount> <description>\
+                   \n--expenses\
+                   \n--expense_log\
+                   \n--reset")
 
 @bot.command()
 async def spend(ctx, date: str, category: str, amount: float, description: str):
-    t.insert_expense(date, category, amount, description)
+    if t.insert_expense(date, category, amount, description) < 0:
+        await ctx.send(f"Invalid category. Please run --add_cat <category> to add a category.")
+        return
     await ctx.send(f"You've spent ${amount} on {category}.")
+    return
+
+@bot.command()
+async def add_cat(ctx, category: str):
+    t.add_cat(category)
+    CATEGORIES.append(category)
+    total_expenses = t.get_expenses()
+    await ctx.send(f"Category {category} added.\nTotal expenses: \n{total_expenses}")
+# TODO: create a rem_cat that removes categories (loss of data is fine)
+
+# TODO: create an add_budget
 
 @bot.command()
 async def expenses(ctx):
@@ -34,8 +54,9 @@ async def expense_log(ctx):
     await ctx.send(f"Expense Log: \n{total_expenses}")
 
 @bot.command()
-async def reset(ctx):
-    t.new_cycle()
+async def reset(ctx, new_month):
+    # TODO: add a command to make a new budget for the month
+    t.new_cycle(new_month)
     total_expenses = t.get_expenses()
     await ctx.send(f"---Reset Expense Log---\nTotal expenses: \n{total_expenses}")
 
--- tracking.py
+++ tracking.py
@@ -6,16 +6,45 @@ CATEGORIES = ["Groceries", "Clothes", "Hygiene", "Food", "Events"]
 
 conn = sqlite3.connect('expenses.db')  # Connect to your database file
 c = conn.cursor()
-c.execute('CREATE TABLE IF NOT EXISTS expenses (date VARCHAR(50), category VARCHAR(50), amount DECIMAL(10, 2), description VARCHAR(255))')
+c.execute('CREATE TABLE IF NOT EXISTS expenses (\
+          date VARCHAR(50), category VARCHAR(50), \
+          amount DECIMAL(10, 2), description VARCHAR(255))')
 
-def new_cycle():
+budget_conn = sqlite3.connect('budgets.db')  # Connect to your database file
+b = budget_conn.cursor()
+b.execute('CREATE TABLE IF NOT EXISTS budgets (month VARCHAR(50), balance DECIMAL(10,2))')
+
+for i in CATEGORIES:
+    # Check if the column exists
+    b.execute(f"PRAGMA table_info('budgets')")
+    columns = b.fetchall()
+    column_names = [column[1] for column in columns]
+
+    if i not in column_names:
+        # Add the column to the table
+        b.execute(f"ALTER TABLE budgets ADD COLUMN {i} DECIMAL(10, 2)")
+
+def new_cycle(new_month):
     c.execute('DELETE FROM expenses')
     conn.commit()
 
 
 def insert_expense(date, category, amount, description):
-    c.execute(f'INSERT INTO expenses (date, category, amount, description) VALUES {(date, category, amount, description)}')
+    if category not in CATEGORIES:
+        return -1
+    c.execute(f'INSERT INTO expenses (date, category, amount, description) \
+              VALUES {(date, category, amount, description)}')
     conn.commit()
+    return 0
+
+def add_cat(category):
+    CATEGORIES.append(category)
+    # Check if the column exists
+    b.execute(f"PRAGMA table_info('budgets')")
+    columns = b.fetchall()
+    column_names = [column[1] for column in columns]
+    if category not in column_names:
+        b.execute(f"ALTER TABLE budgets ADD COLUMN {category} DECIMAL(10, 2)")
 
 def get_expense_log():
     c.execute('SELECT amount, category, description, date FROM expenses')
@@ -28,10 +57,25 @@ def get_expense_log():
 def get_expenses():
     expenses = ""
     for cat in CATEGORIES:
-        c.execute(f'SELECT amount, category, description, date FROM expenses WHERE category = \'{cat}\'')
+        c.execute(f'SELECT amount, category, description, date \
+                  FROM expenses WHERE category = \'{cat}\'')
         all = c.fetchall()
         total = 0
         for i in all:
             total += i[0]
         expenses += f'Spent ${total} on {cat}\n'
     return expenses
+
+# def set_budget(month, amt_list):
+
+
+def get_budget(month):
+    budget = ""
+    c.execute(f'SELECT * FROM budget WHERE month = \'{month}\'')
+    all = c.fetchall()
+    # for cat in CATEGORIES:
+    #     total = 0
+    #     for i in all:
+    #         total += i[0]
+    #     expenses += f'Spent ${total} on {cat}\n'
+    return all
